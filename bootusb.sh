0f6edaa07c49bf4ff9a144a530dd3058483db211
Fhurqaan Hamid <fhurqaan@gmail.com>
2022-07-24 17:19:13 +0800

    Boot Slackware from USB

--- bootusb.sh
+++ bootusb.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 #
 # I got the steps to acomplish booting Slackware from usb from
 # https://www.youtube.com/watch?v=ddjw_yPvxNY 
@@ -17,19 +17,31 @@
 # have installed.
 #
 # Remember: money is the root of all evil....send $10 for more info!
+#
+# COPY THIS SCRIPT TO YOUR SLACKWARE BOOT USB, 
+# AFTER INSTALL SLACKWARE, DO NOT EXIT THE INSTALLER
+# RUN THIS IS SCRIPT
+#
 
-# check parameters
+# set max partition to check...
 MAX_PART=2
+
 echo Make your Slackware boot from USB
 echo version 2.0.0
 echo No copyrights, copylefts...or copywrongs
+
+# check parameters
 [[ $1 == "" ]] && echo -e "\nUsage:-\n./bootusb.sh /dev/sdX [update]\nWhere X is drive partition to boot.\n[update] is optional to update to new kernel version you downloaded, defaults to 5.15.27\n" && exit 
+
+# Check if the drive specified is found
 blkid $1 2>&1 > /dev/null
 if [ $? -gt 0 ] 
 then
 	echo $1 not found..Please enter valid drive
 	exit
 fi
+
+# Check for second parameter
 
 case "$2" in
 	update)
@@ -46,23 +58,27 @@ esac
 
 # MAX_PART is set to 2, if your have more partitions, increase the 
 # number. I dont have more that 2 parts, so adjust script accordingly.
+# Search for ext4,swap and their UUIDs
 for (( n=1; n<=$MAX_PART; n++ ))
 do
 	rtdrv=$1$n
 	# set variables
 	ptype=`blkid $1$n | sed 's/.* TYPE=/\l/' | cut -d\" -f2`
-	if [[ $ptype == "ext4" ]]
-	then
-		mntdrv=$1$n
-		duuid=`blkid $rtdrv | sed 's/.*: UUID=/\l/'| cut -d\" -f2` 
-		puuid=`blkid $rtdrv | sed 's/.*PARTUUID=/\l/'| cut -d\" -f2` 
-		wrkdv=${1:4}$n
-	else
-		swpuuid=`blkid $rtdrv | sed 's/.*: UUID=/\l/'| cut -d\" -f2` 
-		swppuuid=`blkid $rtdrv | sed 's/.*PARTUUID=/\l/'| cut -d\" -f2` 
-	fi
+	case $ptype in
+		ext4)
+			mntdrv=$1$n
+			duuid=`blkid $rtdrv | sed 's/.*: UUID=/\l/'| cut -d\" -f2` 
+			puuid=`blkid $rtdrv | sed 's/.*PARTUUID=/\l/'| cut -d\" -f2` 
+			wrkdv=${1:4}$n
+			;;
+		swap)
+			swpuuid=`blkid $rtdrv | sed 's/.*: UUID=/\l/'| cut -d\" -f2` 
+			swppuuid=`blkid $rtdrv | sed 's/.*PARTUUID=/\l/'| cut -d\" -f2` 
+			;;
+		*) ;;
+	esac
 done
-#
+
 # check working dir
 if [[ ! -d $wrkdv ]] 
 then
@@ -71,46 +87,45 @@ then
 fi
 
 # mount drive
-mount /$mntdrv /$wrkdv
+mount $mntdrv $wrkdv
 
 # Save UUID for boot and swap partition to file
-echo "UUID=$swpuuid	swap	swap	defaults	0	0" > fstabentry 
-echo "UUID=$duuid	/	ext4	defaults	1	1" >> fstabentry 
+echo "UUID=$swpuuid	swap	swap	defaults	0	0" > fstab.tmp 
+echo "UUID=$duuid	/	ext4	defaults	1	1" >> fstab.tmp 
 echo Creating fstab.new file in $wrkdv/etc directory  
-# join the file to the fstab file
-#cat fstabentry /etc/fstab > fstab.new
 
-cat fstabentry $wrkdv/etc/fstab > fstab.new
-# comment out line 3 and 4 
+# join the file to the fstab file
+cat fstab.tmp $wrkdv/etc/fstab > fstab.new
+
+# comment out line 3 and 4 in the defaults /etc/fstab file
 sed -i '3,4s/^\/dev/#\/dev/g' fstab.new
-#mv $wrkdv/etc/fstab $wrkdv/etc/fstab.org
-#mv fstab.new $wrkdv/etc/fstab
-rm fstabentry
+
+# remove temp file
+rm fstab.tmp
 echo Go thru the new fstab file in your current directory....then copy to the /etc/fstab directory.
 echo Lines 3 and 4 are commented by default.
 echo These are the default mountpoint for swap and sda partition.
+
+# set paths for mkinitrd and lilo.conf
 initfile=$wrkdv/etc/mkinitrd.conf
 lilofile=$wrkdv/etc/lilo.conf
-#
-echo removing /proc /sys /dev
+
 # housekeeping
+echo removing /proc /sys /dev
 rm -rf $wrkdv/proc/*
 rm -rf $wrkdv/sys/*
 rm -rf $wrkdv/dev/*
-##
+
 echo remounting /proc /sys /dev
 mount --bind /proc $wrkdv/proc
 mount --bind /sys  $wrkdv/sys
 mount --bind /dev  $wrkdv/dev
-
-# run script
-
 rm -rf $wrkdv/tmp/initrd-tree
 rm -f $wrkdv/boot/initrd.gz
 
 # create mkinitrd.conf
+# edit to suit your needs
 echo "Creating $initfile"
-#echo "KERNEL_VERSION=\"5.15.19\""			 > $initfile
 echo "KERNEL_VERSION=\"$kernel\""			 > $initfile
 echo "SOURCE_TREE=\"/tmp/initrd-tree\"" 	>> $initfile
 echo "OUTPUT_IMAGE=\"/boot/initrd.gz\"" 	>> $initfile
